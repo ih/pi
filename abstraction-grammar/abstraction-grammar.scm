bec44b85a265d004568aa60294feb82f13b7ef97
ih <irvin.hwang@gmail.com>
2011-03-15 14:08:14 -0700

    grammar and rule data abstracted, possibly abstract options in rules

--- abstraction-grammar/abstraction-grammar.scm
+++ abstraction-grammar/abstraction-grammar.scm
@@ -1,15 +1,16 @@
 (library (abstraction-grammar)
-         (export make-start-rule select apply-option make-rule START-SYMBOL make-grammar make-option rule->define)
+         (export make-start-rule select apply-option make-rule START-SYMBOL make-grammar make-option rule->define make-options grammar->sexpr)
          (import (rnrs)
                  (abstract)
                  (_srfi :1)
-                 (only (church readable-scheme) uniform-draw))
+                 (only (church readable-scheme) uniform-draw pair))
 ;;;select will eventually take an optional parameter, this can vary depending on what select calls, e.g. if select is a multinomial than the optional parameter could be a list of probabilities
 ;;;select :: list -> item in the list
          (define START-SYMBOL 'S)
          
          (define select uniform-draw)
 
+         ;;options are sexprs for thunks
          (define (make-rule rule-name options)
            (list 'rule rule-name options))
 
@@ -17,23 +18,32 @@
          (define rule->options third)
          (define rule->options-list third)
 
-         ;;turn a rule into a define
+         ;;get the sexpr representation of a rule
          (define (rule->define rule)
            `(define (,(rule->name rule)) (apply-option ,@(rule->options-list rule))))
 
          (define (make-option sexpr)
            `(lambda () ,sexpr))
 
-         ;;every rule takes a list of sexpressions that will be the options for the rhs
-         (define (make-start-rule rhs-sexprs)
-           (make-rule START-SYMBOL rhs-sexprs))
+         (define (make-options sexpr-list)
+           (map make-option sexpr-list))
 
+         ;;every rule takes a list of sexpressions that will be the options for the rhs
+         (define (make-start-rule options)
+           (make-rule START-SYMBOL options))
 
          (define (apply-option . options)
            ((select options)))
 
-         (define (make-grammar rule-list)
-           `(let () ,@rule-list  (lambda () (,START-SYMBOL))))
+         (define (make-grammar start-rule rule-list)
+           (list 'grammar (rule->name start-rule) (pair start-rule rule-list)))
+
+         (define grammar->rule-list third)
+
+         (define grammar->start-symbol second)
+         
+         (define (grammar->sexpr grammar)
+           `(let () ,@(map rule->define (grammar->rule-list grammar))  (lambda () (,(grammar->start-symbol grammar)))))
          )
 
 
--- abstraction-grammar/tests/abstraction-grammar-tests.scm
+++ abstraction-grammar/tests/abstraction-grammar-tests.scm
@@ -3,38 +3,39 @@
         (abstraction-grammar)
         (srfi :78))
 ;;;make-rule tests
-(check (rule->define (make-rule 'F1 (map make-option '((list 'a (list 'a (V25) (V25)))))))
+(check (rule->define (make-rule 'F1 (make-options '((list 'a (list 'a (V25) (V25)))))))
        =>
        '(define (F1) (apply-option (lambda () (list 'a (list 'a (V25) (V25)))))))
 
 (check (eval `(let ((V25 (lambda () (list 'b))))
-                ,(rule->define (make-rule 'F1 (map make-option '((list 'a (list 'a (V25) (V25))))))) (F1))
+                ,(rule->define (make-rule 'F1 (make-options '((list 'a (list 'a (V25) (V25))))))) (F1))
              (interaction-environment))
        =>
        '(a (a (b) (b))))
 
 ;;;make-start-rule tests
-;; (let* ((F1 (lambda () (+ 1 1))))
-;;   (check (make-start-rule (rhs-options '(F1))) => `(define (,START-SYMBOL) (apply-option (lambda () (F1))))))
+(let* ((F1 (lambda () (+ 1 1))))
+  (check (rule->define (make-start-rule (make-options '((F1))))) => `(define (,START-SYMBOL) (apply-option (lambda () (F1))))))
 
-;; (check (eval `(let ((F1 (lambda () (+ 1 1)))) ,(make-start-rule (rhs-options '(F1))) (,START-SYMBOL)) (interaction-environment)) => 2)
+(check (eval `(let ((F1 (lambda () (+ 1 1)))) ,(rule->define (make-start-rule (make-options '((F1))))) (,START-SYMBOL)) (interaction-environment)) => 2)
 
-;; ;;;make-grammar tests
-;; (let* ([start-rule (make-start-rule (rhs-options '(F1)))]
-;;        [rule-F1 (make-rule 'F1 (rhs-options '`(a (a (,(V25)) (,(V25))))))]
-;;        [rule-V25 (make-rule 'V25 (rhs-options ''b))])
-;;  (check (make-grammar (list start-rule rule-F1 rule-V25))
-;;         =>
-;;         `(let ()
-;;            (define (,START-SYMBOL) (apply-option (lambda () (F1))))
-;;            (define (F1) (apply-option (lambda () `(a (a (,(V25)) (,(V25)))))))
-;;            (define (V25) (apply-option (lambda () 'b)))
-;;            (lambda () (,START-SYMBOL)))))
-;; (let* ([start-rule (make-start-rule (rhs-options '(F1)))]
-;;        [rule-F1 (make-rule 'F1 (rhs-options '`(a (a (,(V25)) (,(V25))))))]
-;;        [rule-V25 (make-rule 'V25 (rhs-options ''b))]
-;;        [grammar (make-grammar (list start-rule rule-F1 rule-V25))])
-;;   (check ((eval grammar (interaction-environment))) => '(a (a (b) (b)))))
+;;;make-grammar tests
+(let* ([start-rule (make-start-rule (make-options '((F1))))]
+       [rule-F1 (make-rule 'F1 (make-options '((list 'a (list 'a (V25) (V25))))))]
+       [rule-V25 (make-rule 'V25 (make-options '((list 'b))))])
+ (check (grammar->sexpr (make-grammar start-rule (list rule-F1 rule-V25)))
+        =>
+        `(let ()
+           (define (,START-SYMBOL) (apply-option (lambda () (F1))))
+           (define (F1) (apply-option (lambda () (list 'a (list 'a (V25) (V25))))))
+           (define (V25) (apply-option (lambda () (list 'b))))
+           (lambda () (,START-SYMBOL)))))
+
+(let* ([start-rule (make-start-rule (make-options '((F1))))]
+       [rule-F1 (make-rule 'F1 (make-options '((list 'a (list 'a (V25) (V25))))))]
+       [rule-V25 (make-rule 'V25 (make-options '((list 'b))))]
+       [grammar-sexpr (grammar->sexpr (make-grammar start-rule (list rule-F1 rule-V25)))])
+     (check ((eval grammar-sexpr (interaction-environment))) => '(a (a (b) (b)))))
 
 ;;;abstraction->grammar tests
 
