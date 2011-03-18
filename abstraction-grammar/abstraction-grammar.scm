8fb02380a5667240ff93c89729d112f04b6a2119
ih <irvin.hwang@gmail.com>
2011-03-18 15:28:35 -0700

    tests up to abstraction->rules passing, all code for program->grammar written

--- abstraction-grammar/abstraction-grammar.scm
+++ abstraction-grammar/abstraction-grammar.scm
@@ -1,21 +1,23 @@
 (library (abstraction-grammar)
-         (export make-start-rule select apply-option make-rule START-SYMBOL make-grammar make-option rule->define make-options grammar->sexpr)
+         (export make-start-rule select apply-option make-rule START-SYMBOL make-grammar make-option rule->define make-options grammar->sexpr create-rule-applications body->options program->start-rule variable->rule abstraction->rules rule-application?)
          (import (rnrs)
                  (abstract)
                  (_srfi :1)
-                 (only (church readable-scheme) uniform-draw pair))
+                 (only (church readable-scheme) uniform-draw pair rest)
+                 (util))
 ;;;select will eventually take an optional parameter, this can vary depending on what select calls, e.g. if select is a multinomial than the optional parameter could be a list of probabilities
 ;;;select :: list -> item in the list
          (define START-SYMBOL 'S)
          
          (define select uniform-draw)
 
-         ;;options are sexprs for thunks
+         ;;the rule-name corresponds w/ the lhs of a production rule and the options are the possible expansions of the rule i.e. they make up the rhs of a production rule
          (define (make-rule rule-name options)
            (list 'rule rule-name options))
 
          (define rule->name second)
          (define rule->options third)
+         ;;since options is implmented as a list rule->options-list merely returns options, but this abstraction exists in case the underlaying data structure for options changes
          (define rule->options-list third)
 
          ;;get the sexpr representation of a rule
@@ -41,10 +43,73 @@
          (define grammar->rule-list third)
 
          (define grammar->start-symbol second)
-         
+
          (define (grammar->sexpr grammar)
            `(let () ,@(map rule->define (grammar->rule-list grammar))  (lambda () (,(grammar->start-symbol grammar)))))
-         )
+;;;conversion from programs found by abstraction into grammars
+         (define (program->grammar program)
+           (let* ([start-rule (program->start-rule program)]
+                  [rules (program->rules program)])
+            (make-grammar start-rule rules)))
+
+
+         (define (program->start-rule program)
+           (let* ([options (body->options (program->body program))])
+             (make-start-rule options)))
+
+         ;;assumes program has a body which is some sort of function over a list of sexprs e.g. uniform-draw
+         ;;the sexprs that are abstraction function applications are returned as thunk applications
+         ;;everything else is returned as is
+         (define (body->options program-body)
+           (let* ([sexprs (rest program-body)]
+                  [thunkified-sexprs (create-rule-applications sexprs)])
+             (make-options thunkified-sexprs)))
+
+         (define (create-rule-applications sexprs)
+           (define (create-rule-application sexpr)
+             (if (list? sexpr)
+                 (list (first sexpr))
+                 (list sexpr)))
+           (sexp-search rule-application? create-rule-application sexprs))
+
+         ;;assumes abstractions and only abstractions have name of the form '[FUNC-SYMBOL][Number]
+         (define (rule-application? sexpr)
+           (if (non-empty-list? sexpr)
+               (func? (first sexpr))
+               (var? sexpr)))
+
+         ;;abstraction->rules returns a list of rules because it returns both the rule for the abstraction function
+         ;;as well as rules for any variables of the abstraction function
+         (define (program->rules program)
+           (let* ([abstractions (program->abstractions program)])
+             (concatenate (map (curry abstraction->rules program) abstractions))))
+
+         ;;consider making a special function make-abstraction-rule that calls make-rule, the difference would be it takes only a single option rather than a list of options
+         (define (abstraction->rules program abstraction)
+           (let* ([name (abstraction->name abstraction)]
+                  [option (make-options (list (create-rule-applications (abstraction->pattern abstraction))))]
+                  [variable-rules (map (curry variable->rule program abstraction) (abstraction->vars abstraction))])
+             (pair (make-rule name option) variable-rules)))
+
+         ;;need program to find all applications of the abstraction (e.g. '(F1 a b c) '(F1 1 3 a) would be applications of F1
+         ;;need abstraction to know which argument variable is in the in the applications (and to find the applications)
+         ;;if the signature for the abstraction is '(F1 V1 V2 V3) and the target variable is V2 then we'll make 'b and 3 options
+         ;;eventually we can make this more compact be removing duplicate instances and drawing from a multinomial where the probability is based on the number of instances (but perhaps we should allow recursive compressions to deal with this?)
+         ;;there are subtleties between number of applications and number of recursive applications of a function...maybe not
+         (define (variable->rule program abstraction variable)
+           (let* ([abstraction-applications (program->abstraction-applications program abstraction)]
+                  [variable-position (abstraction->variable-position abstraction variable)]
+                  [variable-instances (create-rule-applications (map (curry ith-argument variable-position) abstraction-applications))])
+             (make-rule variable (make-options variable-instances))))
+
+         ;;i+1 because the first element is the function name
+         (define (ith-argument i function-application)
+           (list-ref function-application (+ i 1))))
+
+
+
+
+
 
 
 
--- abstraction-grammar/tests/abstraction-grammar-tests.scm
+++ abstraction-grammar/tests/abstraction-grammar-tests.scm
@@ -37,15 +37,68 @@
        [grammar-sexpr (grammar->sexpr (make-grammar start-rule (list rule-F1 rule-V25)))])
      (check ((eval grammar-sexpr (interaction-environment))) => '(a (a (b) (b)))))
 
-;;;abstraction->grammar tests
+;;;abstraction program->grammar tests
+;;create-rule-applications
+(let* ([sexprs '((F8 'b) (F8 'c) (F8 'd))])
+   (check (create-rule-applications sexprs) => '((F8) (F8) (F8))))
+;;body->options
+(let* ([body '(uniform-draw (F8 'b) (F8 'c) (F8 'd))])
+  (check (body->options body) => '((lambda () (F8)) (lambda () (F8)) (lambda () (F8)))))
+;;program->start-rule
+(let* ([program (sexpr->program '(let ()
+                                   (define F8
+                                     (lambda (V25) (list 'a (list 'a (list V25) (list V25)))))
+                                   (uniform-draw (F8 'b) (F8 'c) (F8 'd))))])
+  (check (rule->define (program->start-rule program)) => `(define (,START-SYMBOL) (apply-option (lambda () (F8)) (lambda () (F8)) (lambda () (F8))))))
+;;;
+;;variable->rule
+(let* ([program (sexpr->program '(let ()
+                                   (define F8
+                                     (lambda (V25) (list 'a (list 'a (list V25) (list V25)))))
+                                   (uniform-draw (F8 'b) (F8 'c) (F8 'd))))]
+       [abstraction (define->abstraction '(define F8
+                                            (lambda (V25) (list 'a (list 'a (list V25) (list V25))))))])
+  (check (rule->define (variable->rule program abstraction 'V25))
+         =>
+         '(define (V25) (apply-option (lambda () 'b) (lambda () 'c) (lambda () 'd)))))
+;;abstraction->rules
+(let* ([program (sexpr->program '(let ()
+                                   (define F8
+                                     (lambda (V25) (list 'a (list 'a (list V25) (list V25)))))
+                                   (uniform-draw (F8 'b) (F8 'c) (F8 'd))))]
+       [abstraction (define->abstraction '(define F8
+                                            (lambda (V25) (list 'a (list 'a (list V25) (list V25))))))])
+  (check (map rule->define (abstraction->rules program abstraction))
+         =>
+         '((define (F8) (apply-option (lambda () (list 'a (list 'a (list (V25)) (list (V25)))))))
+           (define (V25) (apply-option (lambda () 'b) (lambda () 'c) (lambda () 'd))))))
 
+;;program->rules
+;; (let* ([program (sexpr->program '(let ()
+;;                                    (define F8
+;;                                      (lambda (V25) (list 'a (list 'a (list V25) (list V25)))))
+;;                                    (uniform-draw (F8 'b) (F8 'c) (F8 'd))))])
+;;   (check (map rule->define (program->rules program))
+;;          =>
+;;          '((define (F8) (apply-option (lambda () (list 'a (list 'a (V25) (V25))))))
+;;            (define (V25) (apply-option (lambda () (list 'b)) (lambda () (list 'c)) (lambda () (list 'd)))))))
+;; ;;program->grammar
+;; (let* ([program (sexpr->program '(let ()
+;;                                    (define F8
+;;                                      (lambda (V25) (list 'a (list 'a (list V25) (list V25)))))
+;;                                    (uniform-draw (F8 'b) (F8 'c) (F8 'd))))])
+;;   (check (grammar->sexpr (program->grammar program))
+;;          =>
+;;          `(let ()
+;;            (define (,START-SYMBOL) (apply-option (lambda () (F8))))
+;;            (define (F8) (apply-option (lambda () (list 'a (list 'a (V25) (V25))))))
+;;            (define (V25) (apply-option (lambda () (list 'b)) (lambda () (list 'c)) (lambda () (list 'd))))
+;;            (lambda () (,START-SYMBOL)))))
 
-
-;;;multiple possible trees 
-;; (let* ([start-rule (make-start-rule '((F1)))]
-;;        [rule-F1 (make-function-rule 'F1 '(a (a (V25) (V25))))]
-;;        [rule-V25 (make-variable-rule 'V25 '(b c d))]
-;;        [grammar (make-grammar start-rule (rhs-options rule-F1 rule-V25))])
-;;  (check-member ((eval grammar)) => '(a (a (b) (b)))))
+;; ;;;multiple possible trees 
+;; ;; (let* ([start-rule (make-start-rule '((F1)))]
+;; ;;        [rule-F1 (make-function-rule 'F1 '(a (a (V25) (V25))))]
+;; ;;        [rule-V25 (make-variable-rule 'V25 '(b c d))]
+;; ;;        [grammar (make-grammar start-rule (rhs-options rule-F1 rule-V25))])
+;; ;;  (check-member ((eval grammar)) => '(a (a (b) (b)))))
 (check-report)
-(exit)
